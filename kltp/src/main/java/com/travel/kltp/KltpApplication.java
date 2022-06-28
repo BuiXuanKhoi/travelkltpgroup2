c5e61e5f6c5cfa7302fb167d6a2bc0441f3a36bb
Bui Xuan Khoi <khoibuiqn1011@gmail.com>
2022-06-29 02:52:45 +0700

    first commit

---
+++ kltp/src/main/java/com/travel/kltp/KltpApplication.java
@@ -0,0 +1,13 @@
+package com.travel.kltp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KltpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KltpApplication.class, args);
+	}
+
+}
---
+++ kltp/src/test/java/com/travel/kltp/KltpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.kltp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
